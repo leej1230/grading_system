4086bf27d055808154b476aed50106f7ab267b69
jaewoo <leejaewoo1230@gmail.com>
2023-08-10 15:40:07 +0900

    implemented unit test for util functions

---
+++ test_module.py
@@ -0,0 +1,51 @@
+import unittest
+from unittest import mock
+import utils
+import pandas as pd
+
+data = {
+    'sid': [123, 456, 789],
+    'score': [None, 3, None],
+    'comment': ['', 'Hello World!', '']
+}
+
+class TestUtilFunctions(unittest.TestCase):
+
+    def test_load_pdf_list(self):
+        with mock.patch('os.listdir') as mocked_listdir:
+            mocked_listdir.return_value = ['123@test123test@course.pdf', '789@test789test@course.pdf']
+            res = utils.load_pdf_list()
+        ans = {'123':'123@test123test@course.pdf', '789':'789@test789test@course.pdf'}
+        self.assertEqual(res, ans)
+
+    def test_sid_list(self):
+        with mock.patch('pandas.read_csv') as mocked_read_csv:
+            mocked_read_csv.return_value = pd.DataFrame(data)
+            res = utils.sid_list()
+        ans = [123, 456, 789]
+        self.assertEqual(res, ans)
+
+    def test_sid_ungraded_list(self):
+        with mock.patch('pandas.read_csv') as mocked_read_csv:
+            mocked_read_csv.return_value = pd.DataFrame(data)
+            res = utils.sid_ungraded_list()
+        ans = [123, 789]
+        self.assertEqual(res, ans)
+
+    def test_sid_graded_list(self):
+        with mock.patch('pandas.read_csv') as mocked_read_csv:
+            mocked_read_csv.return_value = pd.DataFrame(data)
+            res = utils.sid_ungraded_list()
+        ans = [456]
+        self.assertEqual(res, ans)
+    
+    def test_update_grade(self):
+        with mock.patch('pandas.read_csv') as mocked_read_csv:
+            mocked_read_csv.return_value = pd.DataFrame(data)
+            with mock.patch('pandas.DataFrame.to_csv') as mocked_to_csv:
+                utils.update_grade(123,'5','test')
+
+        mocked_to_csv.assert_called_once_with('save.csv', index=False)
+
+if __name__ == '__main__':
+    unittest.main()
--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from os import listdir
+import os
 
 # Read all pdf under directory 'pdfs' and return dict
 # Input: None
@@ -7,7 +7,7 @@ from os import listdir
 def load_pdf_list():
     sid_to_file_name = {}
 
-    for file_name in listdir('pdfs'):
+    for file_name in os.listdir('pdfs'):
         sid = file_name.split('@')[0]
         sid_to_file_name[sid] = file_name
 
@@ -23,9 +23,16 @@ def sid_list():
 # Returns list of sid that haven't been graded
 # Input: None
 # Output: List of student ID
-def sid_list():
+def sid_ungraded_list():
     grade_csv = pd.read_csv('save.csv')
-    return grade_csv[grade_csv.score != None].values.tolist()
+    return grade_csv[grade_csv.score.isna()]['sid'].values.tolist()
+
+# Returns list of sid that have been graded
+# Input: None
+# Output: List of student ID
+def sid_graded_list():
+    grade_csv = pd.read_csv('save.csv')
+    return grade_csv[grade_csv.score.isna()]['sid'].values.tolist()
 
 # Updates score and comment of sid row provided on csv and saves
 # Input: sid(int), score(str), comment(str)
@@ -36,5 +43,3 @@ def update_grade(sid:int, score:str, comment:str):
     grade_csv.loc[mask, 'score'] = str(score)
     grade_csv.loc[mask, 'comment'] = comment
     grade_csv.to_csv('save.csv', index=False)
-
-print(sid_list())
